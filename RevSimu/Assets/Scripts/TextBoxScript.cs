c2f7e3144eb0fcd552ae5fe120af31a66b7e7f03
Monir Imamverdi <monir.imamverdi@gmail.com>
2015-01-24 12:29:59 -0700

    Template with text boxes ready to use
    One script controls all text boxes now. We can have it read from JSON
    inputs.

--- RevSimu/Assets/Scripts/TextBoxScript.cs
+++ RevSimu/Assets/Scripts/TextBoxScript.cs
@@ -4,13 +4,16 @@ using System.Collections;
 using System.Text;
 using System.IO;
 
-public class LeftNameBoxScript : MonoBehaviour {
+public class TextBoxScript : MonoBehaviour {
 	
-	public Text text; // reference to Text component
+	public Text rightNameText; // reference to text component in right name box
+	public Text leftNameText; // reference to text component in left name box
+	public Text convoText; // reference to text component in dialogue box
 	
 	private bool LoadFromFile(string filename) {
 		// Function used to read text from a file to display
 		// inside our conversation/dialogue box.
+		// NOT USED AT THE MOMENT.
 		
 		try
 		{
@@ -44,11 +47,21 @@ public class LeftNameBoxScript : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start () {
-		text = 	GetComponent <Text> ();
+		// Find and associate text box components
+		GameObject convoGO = GameObject.Find("ConvoText");
+		convoText = convoGO.GetComponent <Text> ();
+
+		GameObject rightNameGO = GameObject.Find("RightNameText");
+		rightNameText = rightNameGO.GetComponent <Text> ();
+
+		GameObject leftNameGO = GameObject.Find("LeftNameText");
+		leftNameText = leftNameGO.GetComponent <Text> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		text.text = "Bloke";
+		convoText.text = "LOL THIS IS ACTUALLY WORKING!";
+		rightNameText.text = "Ladeh";
+		leftNameText.text = "Bloke";
 	}
 }
