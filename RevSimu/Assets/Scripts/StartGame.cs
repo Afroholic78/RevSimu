5c3a8a845e54812418460cbf3f379dde5e89dead
Braedy Kuzma <puckmaster_84@hotmail.com>
2015-01-24 12:17:32 -0700

    Added main screen.

---
+++ RevSimu/Assets/Scripts/StartGame.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGame : MonoBehaviour {
+	public void startGame() {
+		Debug.Log ("Start game!");
+		Application.LoadLevel ("scene1");
+	}
+
+	public void exitGame() {
+		Debug.Log ("Exit game!");
+		Application.Quit ();
+	}
+}
