2c92db3c580a532b7a5b57b003d783eab648cdbc
Braedy Kuzma <puckmaster_84@hotmail.com>
2015-01-24 15:26:04 -0700

    Added scene controller

---
+++ RevSimu/Assets/Scripts/SceneController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneController : MonoBehaviour {
+	// Use name of scene to choose different starting nodes..
+	public string getStartNode() {
+		string name = Application.loadedLevelName;
+		Debug.Log("Getting start of " + name +  ".");
+
+		if (name == "scene1") {
+			return "gamestart";
+		} else if (name == "TemplateScene") { // Debug here
+			return "gamestart";
+		}
+
+		return null;
+	}
+
+	// If we need to start separating json files here would be where
+	public string getJSONFilename() {
+		string name = Application.loadedLevelName;
+		Debug.Log("Getting jsonfile of " + name +  ".");
+		
+		if (name == "scene1") {
+		//	return "gamestart";
+		}
+
+		return "Text/content";
+	}
+}
