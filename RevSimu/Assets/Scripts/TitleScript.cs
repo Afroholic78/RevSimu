65cdd5a1e06ca57347ecccf71dcc870b5e01de2a
Monir Imamverdi <monir.imamverdi@gmail.com>
2015-01-25 12:45:19 -0700

    Empty asset

---
+++ RevSimu/Assets/Scripts/TitleScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScript : MonoBehaviour {
+
+	void OnMouseEnter() {
+		GameObject playGO = GameObject.Find("PlayButton");
+		SpriteRenderer playSprite = playGO.GetComponent<SpriteRenderer> ();
+		Sprite hoverSprite = Resources.Load<Sprite> ("play_btn_hvr");
+		playSprite.sprite = hoverSprite;
+	}
+	
+	void OnMouseDown() {
+		GameObject playGO = GameObject.Find("PlayButton");
+		SpriteRenderer playSprite = playGO.GetComponent<SpriteRenderer> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//OnMouseEnter();	
+	}
+}
