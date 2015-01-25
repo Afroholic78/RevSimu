e60d55e51684fe1c6844a179c60f24d6dce99cb4
Monir Imamverdi <monir.imamverdi@gmail.com>
2015-01-25 13:21:48 -0700

    Title screen ready

---
+++ RevSimu/Assets/Scripts/TitleFader.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleFader : MonoBehaviour {
+
+	private bool startScene = true;
+
+	void FadeIn() {
+		// Fade in scene
+
+		GameObject titelGO = GameObject.Find("GameTitle");
+		Color oldC = titelGO.GetComponent<SpriteRenderer> ().color;
+		
+		if (startScene)
+		{   
+			float b = Mathf.PingPong(Time.time, 1) / 1;
+			Color c = new Color(oldC.r, oldC.g, oldC.b, b);
+			titelGO.GetComponent<SpriteRenderer> ().color = c;
+			if (b >= 0.99f)
+			{
+				startScene = false;
+			}
+		}
+	}
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		FadeIn();
+	}
+}
--- RevSimu/Assets/Scripts/TitleScript.cs
+++ RevSimu/Assets/Scripts/TitleScript.cs
@@ -6,13 +6,25 @@ public class TitleScript : MonoBehaviour {
 	void OnMouseEnter() {
 		GameObject playGO = GameObject.Find("PlayButton");
 		SpriteRenderer playSprite = playGO.GetComponent<SpriteRenderer> ();
-		Sprite hoverSprite = Resources.Load<Sprite> ("play_btn_hvr");
-		playSprite.sprite = hoverSprite;
+		playSprite.sprite = Resources.Load<Sprite> ("Sprites/play_btn_hover");
+
+		Debug.LogWarning("Mouse Over Heart");
+	}
+
+	void OnMouseExit() {
+		GameObject playGO = GameObject.Find("PlayButton");
+		SpriteRenderer playSprite = playGO.GetComponent<SpriteRenderer> ();
+		playSprite.sprite = Resources.Load<Sprite> ("Sprites/play_btn_up");
+		
+		Debug.LogWarning("Mouse Exit Heart");
 	}
 	
 	void OnMouseDown() {
 		GameObject playGO = GameObject.Find("PlayButton");
 		SpriteRenderer playSprite = playGO.GetComponent<SpriteRenderer> ();
+		playSprite.sprite = Resources.Load<Sprite> ("Sprites/play_btn_click");
+		Application.LoadLevel(Application.loadedLevel + 1); // Load next scene
+		Debug.LogWarning("Mouse Click Heart");
 	}
 
 	// Use this for initialization
@@ -22,6 +34,6 @@ public class TitleScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		//OnMouseEnter();	
+
 	}
 }
